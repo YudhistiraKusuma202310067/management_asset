9ab4e4009c81c5a697829d03759b2a6a444ed37a
Axel Frans Silalahi <axelfrans93@gmail.com>
2025-06-12 09:47:15 +0700

    Create Model
    - User
    - Employee
    - Role

---
+++ src/main/java/com/management_asset/api/Model/Role.java
@@ -0,0 +1,27 @@
+package com.management_asset.api.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_m_role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Integer level;
+
+}
