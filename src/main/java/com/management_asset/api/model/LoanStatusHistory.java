f46c7ef4aaed451fea1ebaa804def8a45abd5e9c
arul14444 <mochamadfachrul14@gmail.com>
2025-06-12 09:45:59 +0700

    Add Model
    - Loaning
    - Loan Status History
    - Loan Status Process

---
+++ src/main/java/com/management_asset/api/model/LoanStatusHistory.java
@@ -0,0 +1,35 @@
+package com.management_asset.api.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_tr_loan_status_history")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoanStatusHistory {
+    private LocalDateTime createdDate;
+
+    // @OneToMany
+    // @JoinColumn(name = "approver_id", referencedColumnName = "id")
+    // private Employee approver;
+
+    @ManyToOne
+    @JoinColumn(name = "loaning_id", referencedColumnName = "id")
+    private Loaning loaning;
+
+    @ManyToOne
+    @JoinColumn(name = "loan_status_process_id", referencedColumnName = "id")
+    private LoanStatusProcess loanStatusProcess;
+
+}
---
+++ src/main/java/com/management_asset/api/model/Loaning.java
@@ -0,0 +1,40 @@
+package com.management_asset.api.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_tr_loaning")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Loaning {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime loanDate;
+    private LocalDateTime returnDate;
+    private String note;
+    // @OneToMany
+    // @JoinColumn(name = "employee_id", referencedColumnName = "id")
+    // private Employee employee;
+
+    // @OneToMany
+    // @JoinColumn(name = "asset_id", referencedColumnName = "id")
+    // private Asset asset;
+    @OneToMany
+    @JoinColumn(name = "loan_status_process_id", referencedColumnName = "id")
+    private LoanStatusProcess loanStatusProcess;
+}
