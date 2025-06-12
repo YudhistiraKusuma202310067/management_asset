7c0fef512c39d66247ee8aa11eafccbe678df61f
Yudhistira Kusuma <202310067@student.ibik.ac.id>
2025-06-12 10:00:55 +0700

    Pulling Branch
     After merge arul axel

--- src/main/java/com/management_asset/model/LoanStatusProcess.java
+++ src/main/java/com/management_asset/model/LoanStatusProcess.java
@@ -1,10 +1,9 @@
-package com.management_asset.api.Model;
+package com.management_asset.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -12,16 +11,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_m_role")
-@Data
+@Table(name = "tb_m_loan_status_process")
+@Data 
 @AllArgsConstructor
 @NoArgsConstructor
-public class Role {
 
+public class LoanStatusProcess {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private Integer level;
-
+    private String loaningStatus;
 }
--- src/main/java/com/management_asset/model/Role.java
+++ src/main/java/com/management_asset/model/Role.java
@@ -1,9 +1,10 @@
-package com.management_asset.api.model;
+package com.management_asset.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -11,14 +12,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_m_loan_status_process")
-@Data 
+@Table(name = "tb_m_role")
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+public class Role {
 
-public class LoanStatusProcess {
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String loaningStatus;
+    private String name;
+    private Integer level;
+
 }
