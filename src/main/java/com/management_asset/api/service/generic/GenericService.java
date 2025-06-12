12a98205065a592f88cf04e17d99f738db86d4c3
arul14444 <mochamadfachrul14@gmail.com>
2025-06-12 11:42:24 +0700

    Add Service
    - Loaning Service
    - Loan Status History Service
    - Loan Status Process Service

---
+++ src/main/java/com/management_asset/api/service/generic/GenericService.java
@@ -0,0 +1,10 @@
+package com.management_asset.api.service.generic;
+
+import java.util.List;
+
+public interface GenericService <T,K>{
+    public List<T> findAll();
+    public T findById(K id);
+    public T save(T model);
+    
+}
