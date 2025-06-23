993cdaf2a74f5bc292e145116b02f818c637d29f
Axel Frans Silalahi <axelfrans93@gmail.com>
2025-06-23 09:22:21 +0700

    Update User Management

--- src/main/java/com/management_asset/api/repository/UserRepository.java
+++ src/main/java/com/management_asset/api/repository/UserRepository.java
@@ -1,12 +1,21 @@
 package com.management_asset.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.management_asset.api.model.Employee;
 import com.management_asset.api.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> { // Integer buat ambil Primary Key nya
 
     User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.role = 1")
+    List<User> getAllManager();
 
 }
--- src/main/java/com/management_asset/api/service/implement/UserManagementService.java
+++ src/main/java/com/management_asset/api/service/implement/UserManagementService.java
@@ -30,23 +30,30 @@ public class UserManagementService {
 
     public Employee register(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
-        employee.setId(employeeDTO.getId());
-        employee.setName(employeeDTO.getName());
-        employee.setEmail(employeeDTO.getEmail());
-        Employee manager = employeeRepository.findById(employeeDTO.getManager()).orElse(null);
-        employee.setManager(manager);
-        Employee savedEmployee = employeeRepository.save(employee);
+        if (employeeRepository.findByName(employeeDTO.getName()) == null
+                && employeeRepository.findByEmail(employeeDTO.getEmail()) == null) {
+            // employee.setId(employeeDTO.getId());
+            employee.setName(employeeDTO.getName());
+            employee.setEmail(employeeDTO.getEmail());
+            // Employee manager =
+            // employeeRepository.findById(employeeDTO.getManager()).orElse(null);
+            Employee manager = employeeRepository.findById(3).orElse(null);
+            employee.setManager(manager);
+            Employee savedEmployee = employeeRepository.save(employee);
 
-        // 2. Simpan User (otomatis username password = nama, role default = 3)
-        User user = new User();
-        user.setUsername(savedEmployee.getName());
-        user.setRandomCode(UUID.randomUUID().toString());
-        user.setPassword(passwordEncoder.encode(savedEmployee.getName()));
-        user.setEmployee(employeeRepository.findById(savedEmployee.getId()).orElse(null));
-        Integer role = roleRepository.findRoleIdByLevel(3); // role default staff
-        user.setRole(roleRepository.findById(role).orElse(null));
-        userRepository.save(user);
-        return savedEmployee;
+            // 2. Simpan User (otomatis username password = nama, role default = 3)
+            User user = new User();
+            user.setUsername(savedEmployee.getName());
+            user.setPassword(passwordEncoder.encode(savedEmployee.getName()));
+            user.setRandomCode(UUID.randomUUID().toString());
+            user.setEmployee(employeeRepository.findById(savedEmployee.getId()).orElse(null));
+            Integer role = roleRepository.findRoleIdByLevel(3); // role default staff
+            user.setRole(roleRepository.findById(role).orElse(null));
+            userRepository.save(user);
+            return savedEmployee;
+        } else {
+            return null;
+        }
     }
 
     public User login(String username, String password) {
@@ -59,8 +66,8 @@ public class UserManagementService {
 
     public Boolean changePassword(String username, String oldPassword, String newPassword) {
         User user = userRepository.findByUsername(username);
-        if (user != null && user.getPassword().equals(oldPassword)) {
-            user.setPassword(newPassword);
+        if (user != null && passwordEncoder.matches(oldPassword, user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(newPassword));
             userRepository.save(user);
             return true;
         }
@@ -73,6 +80,16 @@ public class UserManagementService {
             Integer roleId = roleRepository.findRoleIdByLevel(newLevel);
             user.setRole(roleRepository.findById(roleId).orElse(null));
             userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateUserManager(String name, Integer newManager) {
+        Employee employee = employeeRepository.findByName(name);
+        if (employee != null) {
+            employee.setManager(employeeRepository.findById(newManager).orElse(null));
+            employeeRepository.save(employee);
             return true;
         }
         return false;
